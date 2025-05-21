08bdd306361d4c736c3938f42fdcf39dfb8aa047
finite-codez <finite.plays.inifinite@gmail.com>
2025-05-21 10:08:02 +0530

    Use profile at `framework/base/boot/` instead of the combined one at `framework/base/config`
    This is to decouple the framework and art profiles everywhere.
    
    ```
    Bug: 260067246
    Change-Id: I61af081b402f6e63722a5ae9e22151227a713c95
    Test: running `m`
    Test: Verify PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION for multiple products https://paste.googleplex.com/5573555023773696
    ```

--- device.mk
+++ device.mk
@@ -20,7 +20,7 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # Always preopt extracted APKs to prevent extracting out of the APK
 # for gms modules.
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 USE_DEX2OAT_DEBUG := false
 WITH_DEXPREOPT_DEBUG_INFO := false
